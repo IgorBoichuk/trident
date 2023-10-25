d12802e0038050a48432bd4712e818e10f4f52ba
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-10-25 22:19:09 +0300

    Catalogue

--- src/components/Home/Catalogue.jsx
+++ src/components/Home/Catalogue.jsx
@@ -9,14 +9,14 @@ export const Catalogue = () => {
 	return (
 		<section>
 			<Container>
-				<div className=" border-2">
+				<div className=" border-2 pt-[23px] pb-[68px]">
 					<p className=" font-Lato font-light text-[26px]">Order our catalogue for free</p>
 					<p>Lorem ipsim dolor set amet lorem ipsim dolor set amet lorem ipsim dolor set amet </p>
 					<div className=" relative w-max">
 						<Image src={catalogue} alt="" placeholder="blur" />
 						<Image src={pdf} alt="" className="absolute top-[50%] left-[50%] -translate-y-[50%]" />
 					</div>
-					<button className=" bg-mainBg flex gap-4 text-white p-5 font-medium">
+					<button className=" bg-mainBg flex gap-4 text-white p-5 font-medium m-auto">
 						Order now
 						<MoveRight />
 					</button>
