8f08266562db85c069bc7fadc93b3d3c2f936f42
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-10-25 22:32:20 +0300

    Catalogue

--- src/components/Home/Catalogue.jsx
+++ src/components/Home/Catalogue.jsx
@@ -9,9 +9,11 @@ export const Catalogue = () => {
 	return (
 		<section>
 			<Container>
-				<div className=" border-2 pt-[23px] pb-[68px]">
-					<p className=" font-Lato font-light text-[26px]">Order our catalogue for free</p>
-					<p>Lorem ipsim dolor set amet lorem ipsim dolor set amet lorem ipsim dolor set amet </p>
+				<div className=" border-[1px] border-[#F5F5F5] rounded-[5px] pt-[23px] pb-[68px] shadow-md shadow-grey">
+					<p className=" font-Lato font-light text-[26px] text-center">Order our catalogue for free</p>
+					<p className="font-Lato text-center px-[21px] mb-[24px]">
+						Lorem ipsim dolor set amet lorem ipsim dolor set amet lorem ipsim dolor set amet{" "}
+					</p>
 					<div className=" relative w-max">
 						<Image src={catalogue} alt="" placeholder="blur" />
 						<Image src={pdf} alt="" className="absolute top-[50%] left-[50%] -translate-y-[50%]" />
