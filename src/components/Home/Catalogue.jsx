f1f5adac328556764eb6a4f06a086feac5736158
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-10-25 22:17:08 +0300

    Catalogue

--- src/components/Home/Catalogue.jsx
+++ src/components/Home/Catalogue.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import Image from "next/image";
+import { MoveRight } from "lucide-react";
+import { Container } from "@/app/shared/Container";
+import catalogue from "../../assets/galary/catalogue/catalogue.png";
+import pdf from "../../assets/galary/catalogue/pdf.svg";
 
 export const Catalogue = () => {
-	return <div>Catalogue</div>;
+	return (
+		<section>
+			<Container>
+				<div className=" border-2">
+					<p className=" font-Lato font-light text-[26px]">Order our catalogue for free</p>
+					<p>Lorem ipsim dolor set amet lorem ipsim dolor set amet lorem ipsim dolor set amet </p>
+					<div className=" relative w-max">
+						<Image src={catalogue} alt="" placeholder="blur" />
+						<Image src={pdf} alt="" className="absolute top-[50%] left-[50%] -translate-y-[50%]" />
+					</div>
+					<button className=" bg-mainBg flex gap-4 text-white p-5 font-medium">
+						Order now
+						<MoveRight />
+					</button>
+				</div>
+			</Container>
+		</section>
+	);
 };
