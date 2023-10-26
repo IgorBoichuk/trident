1f8897ae63db9d07a87389e56b6c1076afcce824
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-10-26 22:09:08 +0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className="bg-mainBg text-white">
+			<p>111A T. Shevchenko</p>
+			<p>0054-6272444</p>
+			<p>Info@mail.com</p>
+			<div>
+				<span>© Trident 2023</span>
+				<span>Privacy Policy</span>
+			</div>
+		</div>
+	);
 };
