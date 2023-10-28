72a7e8c5db84c03788f5d849777be75c811abc3b
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-10-29 00:08:51 +0300

    Update SeoHeadline.jsx

--- src/components/Home/SeoHeadline.jsx
+++ src/components/Home/SeoHeadline.jsx
@@ -8,7 +8,7 @@ export const SeoHeadline = () => {
 				<p className="text-[36px] font-thin">Seo headline</p>
 				<p className="text-[30px] mb-6">Seo headline</p>
 				<p className="text-[14px] font-thin leading-6 mb-6">
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys
 					standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
 					make a type specimen book. It has survived not only five centuries, but also the leap into electronic
 					typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
